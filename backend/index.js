0e1d12548f75db10e01e7bb2f9c400ae6fb59dde
Francesco Saverio Nisoli <83357136+Frasavenix@users.noreply.github.com>
2024-11-20 00:22:40 +0000

    Implemented some basic sounding emotion-to-scale functions.

--- backend/index.js
+++ backend/index.js
@@ -30,15 +30,16 @@ app.post("/", async (req, res) => {
                     role: "system", content: "You are an emotionally-intelligent and empathetic agent. "
                         + "You will be given a piece of text, and your task is to identify all the emotions expressed by the writer of the text. "
                         + "The text could be in different languages. You are only allowed to make selections from the following emotions, "
-                        + "and don’t use any other words: [Joy, Sadness, Anger, Anxiety, Calm, Disgust, Arousal, Non-sense]. "
+                        + "and don’t use any other words: [joy, sadness, wonder, transcendence, nostalgia/longing, tenderness, anger, tension, disgust, hilarity, mystery, non-sense]. "
                         + "Only select those ones for which you are reasonably confident that they are expressed in the text. "
-                        + "If no emotion is clearly expressed, select ‘neutral’. Reply with only the list of emotions in form of a JSON array, "
-                        + "separated by comma and with an associated accuracy value (which goes from 0 to 1). The sum of all the accuracies must be 1."
+                        + "If no emotion is clearly expressed, select ‘neutral’. You are asked to also identify the arousal level of the text, "
+                        + "with a value which goes from 0 (calmness) to 1 (complete arousal). Reply with only the list of emotions (plus the arousal) in form of a JSON array, "
+                        + "separated by comma and with an associated accuracy value (which goes from 0 to 1). The sum of all the accuracies (without considering the arousal level) must be 1."
                         + "\nYou are also a precise ambience observer. You have to identify if the text given to you has an ambience "
-                        + "description which suits one of the following words: [Rain, Sea, Beach, City, Mountain, Forest]. "
-                        + "Don't use any other words. If the text does not suit any of these words, select ‘None’."
+                        + "description which suits one of the following words: [rain, wind, sea, beach, city, mountain, forest]. "
+                        + "Don't use any other words. If the text does not suit any of these words, select ‘none’."
                         + "For example purposes, here's how the output should look like:"
-                        + "\n{ 'emotions': [ {'emotion': 'Sadness', 'accuracy': 0.8}, {'emotion': 'Anxiety', 'accuracy': 0.15}, {'emotion': 'Anger', 'accuracy': 0.05} ], 'ambience': 'Sea'}"
+                        + "\n{ 'emotions': [ {'emotion': 'sadness', 'accuracy': 0.8}, {'emotion': 'anxiety', 'accuracy': 0.15}, {'emotion': 'anger', 'accuracy': 0.05} ], 'arousal': 0.67, 'ambience': 'sea'}"
                 }
                 ],
                 max_tokens: 100,
--- frontend/emorse.js
+++ frontend/emorse.js
@@ -13,19 +13,19 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 //MODEL
 
-// Array che contiene il codice Morse per ogni lettera dell'alfabeto in ordine
+// array containing all the morse codes for every alphabet letter
 const morseCodes = [
   ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
   ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
   "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
 ];
 
-// Array per numeri da 0 a 9
+// array for numbers
 const morseNumbers = [
   "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."
 ];
 
-// Array per simboli speciali comuni
+// array for special characters
 const morseSpecialChars = {
   ".": ".-.-.-",
   ",": "--..--",
@@ -45,25 +45,39 @@ const morseSpecialChars = {
   "@": ".--.-."
 };
 
-// Ciclo per aggiungere le lettere dalla 'a' alla 'z' con il corrispondente codice Morse
+// adds all alphabet characters to the map
 for (let i = 97; i <= 122; i++) {
-  const letter = String.fromCharCode(i); // Converte il codice ASCII in lettera
-  const morseCode = morseCodes[i - 97]; // Ottiene il codice Morse dall'array
-  morseMap.set(letter, morseCode); // Aggiunge la lettera e il codice Morse alla mappa
+  const letter = String.fromCharCode(i); // converts ASCII to a letter
+  const morseCode = morseCodes[i - 97]; // gets morse code from the array
+  morseMap.set(letter, morseCode); // adds letter and morse code to the map
 }
 
-// Ciclo per aggiungere i numeri da '0' a '9'
+// adds numbers to the map
 for (let i = 0; i <= 9; i++) {
-  morseMap.set(i.toString(), morseNumbers[i]); // Aggiunge i numeri alla mappa
+  morseMap.set(i.toString(), morseNumbers[i]);
 }
 
-// Aggiungere i simboli speciali alla mappa
+// adds special characters to the map
 Object.entries(morseSpecialChars).forEach(([key, value]) => { morseMap.set(key, value); })
 
+// map relating scales (expressed as semitones distances from the root) and emotions.
 const eModes = {
-  "joy": ["C4", "D4", "E4", "F4", "G4", "A4", "B4"],
-  "sadness": ["C4", "D4", "Eb4", "F4", "G4", "Ab4", "Bb4"]
+  "wonder": ["-10", "-8", "-6", "-5", "-3", "-1", "0", "2", "4", "6", "7", "9", "11"], // lydian
+  "joy": ["-10", "-8", "-7", "-5", "-3", "-1", "0", "2", "4", "5", "7", "9", "11"], // ionian
+  "tenderness": ["-10", "-8", "-7", "-5", "-3", "-2", "0", "2", "4", "5", "7", "9", "10"], // mixolydian
+  "mystery": ["-10", "-8", "-7", "-5", "-4", "-2", "0", "2", "4", "5", "7", "8", "10"], // mixolydian flat 6
+  "nostalgia/longing": ["-10", "-9", "-7", "-5", "-3", "-2", "0", "2", "3", "5", "7", "9", "10"],  // dorian
+  "sadness": ["-10", "-9", "-7", "-5", "-4", "-2", "0", "2", "3", "5", "7", "8", "10"], // eolian
+  "unease": ["-11", "-9", "-7", "-5", "-4", "-2", "0", "1", "3", "5", "7", "8", "10"], // phrygian
+  "tension": ["-11", "-9", "-7", "-6", "-4", "-2", "0", "1", "3", "5", "6", "8", "10"], // locrian
+  "non-sense": ["-11", "-10", "-9", "-8", "-7", "-6", "-5", "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"], //chromatic
+  "hilarity/goofiness": [], // goofy sounds
+  "disgust": [], // disgustive sounds
+  "neutral": [] // normal morse
 }
+
+// the 4th octave's notes frequencies
+const key_roots = [261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392, 415.30, 440, 466.16, 493.88]; 
 
 async function detectEmotion(prompt) {
   return fetch("http://localhost:3000", {
@@ -79,9 +93,9 @@ async function detectEmotion(prompt) {
 
 //VIEW
 
-function playSound(duration) {
+function playSound(duration, frequency) {
   return new Promise((resolve) => {
-    const osc = new Tone.Oscillator(580, "sine").toDestination().start();
+    const osc = new Tone.Oscillator(frequency, "sine").toDestination().start();
     setTimeout(() => {
       osc.stop();
     }, duration);
@@ -89,50 +103,60 @@ function playSound(duration) {
   });
 }
 
-function playDot() {
-  return playSound(dot_duration);
+function playDot(key, scale, velocity) {
+  const frequency = key * Math.pow(2, scale[Math.floor(Math.random() * scale.length)]/12);
+  return playSound(dot_duration/velocity, frequency);
 }
 
-function playLine() {
-  return playSound(line_duration);
+function playLine(key, scale, velocity) {
+  const frequency = key * Math.pow(2, scale[Math.floor(Math.random() * scale.length)]/12);
+  return playSound(line_duration/velocity, key, scale);
+}
+
+function playBass(key){
+  const osc = new Tone.Oscillator(key/4, "sine").toDestination().start();
 }
 
 function playDaHeckAreYouWriting() {
   //TODO: qualche sburrata incredibile per i caratteri che non sono inclusi codice morse.
 }
 
-async function morsify(input_string) {
-  input_string = input_string.toLowerCase(); // Converte l'input a minuscolo
+async function morsify(input_string, emotion, velocity) {
+  let current_key = key_roots[Math.floor(Math.random() * key_roots.length)];
+  let current_scale = eModes[emotion];
+  input_string = input_string.toLowerCase(); // converts the input to lower case
+
+  playBass(current_key);
 
   for (let i = 0; loop || i < input_string.length; i++) {
     const current_char = input_string[i % input_string.length];
 
     if (stop == true) {
-      break; // Esce dal ciclo
+      break; // exits the cycle
     }
 
     if (current_char === " ") {
-      // Spazio: intervallo più lungo per segnalare la fine della parola
-      await delay(word_silence_duration);
-      continue; //Passa al prossimo carattere
+      // space character: waits for a longer time interval (representing the end of a word)
+      await delay(word_silence_duration/velocity);
+      continue; // goes to next input character
     }
 
     const morseCode = morseMap.get(current_char);
     if (morseCode) {
-      // Riproduce ogni simbolo Morse per il carattere attuale
+      // reproduces every Morse symbol for the current character
       for (let j = 0; j < morseCode.length; j++) {
         if (morseCode[j] === ".") {
-          await playDot();
+          await playDot(current_key, current_scale, velocity);
         } else if (morseCode[j] === "-") {
-          await playLine();
+          await playLine(current_key, current_scale, velocity);
         }
-        // Attende un piccolo intervallo tra i simboli nel codice Morse (es. "." o "-")
-        await delay(dot_duration);
+        // waits for a little interval between the morse code symbols (ex. "." o "-")
+        await delay(dot_duration/velocity);
       }
-      // Attende un intervallo medio tra i caratteri nella parola
-      await delay(line_duration);
+      // waits for a "medium" time interval between in-word intervals
+      await delay(line_duration/velocity);
     } else {
-      // Carattere non supportato
+      // the character is not morse coded
       playDaHeckAreYouWriting();
     }
   }
@@ -140,20 +164,21 @@ async function morsify(input_string) {
 
 //CONTROLLER
 
-/*
-detectEmotion("You are an emotionally-intelligent and empathetic agent. You will be given a piece of text, and your task is to identify all the emotions expressed by the writer of the text."
-   + "You are only allowed to make selections from the following emotions, and don’t use any other words: "
-   + "[Joy, Sadness, Anger, Anxiety, Chill, Disgust]. Only select those ones for which you are reasonably confident that they are expressed"
-   + "in the text. If no emotion is clearly expressed, select ‘neutral’. Reply with only the list of emotions, separated by comma.");
-*/
-morsebutton.onclick = function () {
+morsebutton.onclick = async function () {
   stop = false;
 
-  detectEmotion(textfield.value).then((emotions) => {
+  /*detectEmotion(textfield.value).then((emotions) => {
     console.log("detected emotions:", emotions);
-  });
+  });*/
 
-  morsify(textfield.value);
+  //extract emotion
+  /*if(emotion == "non-sense"){
+    morsify(textfield.value, "non-sense", 2);
+    await delay(300);
+    morsify(textfield.value, "non-sense", 2);
+  }*/
+  
+  morsify(textfield.value, "non-sense", 2);
 }
 
 stopbutton.onclick = function () {
